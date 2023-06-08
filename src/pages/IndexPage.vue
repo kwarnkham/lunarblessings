faf3abf22967904015bef1364f60f4cbdcec656e
KWARN KHAM <kwarnkham.moon@gmail.com>
2023-06-08 10:50:15 +0630

    update

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,35 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <div class="row justify-center q-mt-sm">
+      <div class="q-pa-xs col-8">
+        <q-card @click="launch()">
+          <q-card-section>
+            <q-img alt="Begeleider" src="~assets/begeleider.png" />
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { useQuasar } from "quasar";
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+const { dialog } = useQuasar();
+const launch = () => {
+  dialog({
+    title: "Enter the space",
+    position: "top",
+    cancel: true,
+    prompt: {
+      model: "",
+      isValid: (val) => val != "",
+    },
+  }).onOk((value) => {
+    window.open(
+      `https://begeleider.book-mm.com/login?tenant=${value}`,
+      "_blank"
+    );
+  });
+};
 </script>
